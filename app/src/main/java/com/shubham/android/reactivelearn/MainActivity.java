a0e8c09915461422ba7421da0d8564512c5091b5
shubham <it2012.shubham@gmail.com>
2018-07-26 12:12:57 +0530

    used composite disposable with multiple observers

--- app/src/main/java/com/shubham/android/reactivelearn/MainActivity.java
+++ app/src/main/java/com/shubham/android/reactivelearn/MainActivity.java
@@ -5,16 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import io.reactivex.Observable;
-import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
+import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private Disposable disposable;
+    private CompositeDisposable compositeDisposable= new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,27 +30,53 @@ public class MainActivity extends AppCompatActivity {
                 "Dog", "Dove",
                 "Fox", "Frog");
 
-        Observer<String> animalsObserver = getAnimalsObserver();
+        DisposableObserver<String> animalsObserver = getAnimalsObserver();
 
-        animalObservable.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .filter(new Predicate<String>() {
-                    @Override
-                    public boolean test(String s) throws Exception {
-                        return s.toLowerCase().startsWith("b");
-                    }
-                })
-                .subscribe(animalsObserver);
+        DisposableObserver<String> secondAnimalObserver = getAnimalsObserver();
+
+        /*
+         * filter() is used to filter out the animal names starting with `b`
+         */
+        compositeDisposable.add(
+                animalObservable.subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .filter(new Predicate<String>() {
+                            @Override
+                            public boolean test(String s) throws Exception {
+                                return s.toLowerCase().startsWith("b");
+                            }
+                        })
+                        .subscribeWith(animalsObserver));
+
+
+        /*filter() is used to filter out the animals names start with "c"
+         *map() is used to transform all characters to Upper case
+         */
+        compositeDisposable.add(
+                animalObservable.subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .filter(new Predicate<String>() {
+                            @Override
+                            public boolean test(String s) throws Exception {
+                                return s.toLowerCase().startsWith("c");
+                            }
+                        })
+                        .map(new Function<String, String>() {
+
+                            @Override
+                            public String apply(String s) throws Exception {
+
+                                return s.toUpperCase();
+                            }
+                        })
+                        .subscribeWith(secondAnimalObserver)
+        );
+
 
     }
 
-    private Observer<String> getAnimalsObserver() {
-        return new Observer<String>() {
-            @Override
-            public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe: ");
-                disposable = d;
-            }
+    private DisposableObserver<String> getAnimalsObserver() {
+        return new DisposableObserver<String>() {
 
             @Override
             public void onNext(String s) {
@@ -72,6 +99,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        disposable.dispose();
+        compositeDisposable.clear();
     }
 }
